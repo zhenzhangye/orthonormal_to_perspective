8688a84dd860cb703ae63285333a29209390dead
Zhenzhang Ye <yez@in.tum.de>
2019-12-10 17:40:20 +0100

    update demo file

--- demo_ortho2Persp.m
+++ demo_ortho2Persp.m
@@ -8,7 +8,6 @@ load('data/o2p_demo.mat');
 % z_orth: the depth map under orthonormal projection.
 % mask: the mask of the object
 % K: the intrinsic matrix
-% grad_option: the option for finite difference.
 
 N = Depth2Normals_o2p(z_orth, size(mask), mask, 'CNC');
-z_persp = normals2DepthPersp(N, mask, K, grad_option);
+z_persp = normals2DepthPersp(N, mask, K);
